161ac52d87baf84657f5b1b2ad6a98138dec4139
Olivier CHURLAUD <olivier@churlaud.com>
2015-03-09 01:52:57 +0100

    some stats on the stack

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
+#include <ctime>
 #include <linux/input.h>
+#include <vector>
 
 #define EV_PRESSED 1
 #define EV_RELEASED 0
@@ -7,21 +9,46 @@
 
 int main()
 {
+    time_t originTime = time(NULL);
     struct input_event event;
     unsigned int scan_code = 0;
-
+    std::vector<int> charVect(255);
+    double lastTimeDouble(0);
+    double timeDouble(0);
+    double minTime(1000);
+    
     std::ifstream dev2("/dev/input/event0");
     if(dev2.is_open())
     {
+	int i = 0;
         while(dev2.read((char*)&event, sizeof(input_event)))
         {
             if(event.value==EV_PRESSED)
             {
                 scan_code = event.code;
-                printf("%u\n",scan_code);
-                printf("%ld.%06ld\n",event.time.tv_sec,event.time.tv_usec);
-                printf("\n");
+                charVect[scan_code]++;
+		timeDouble = event.time.tv_sec + event.time.tv_usec /1000000.0; 
+		if ((timeDouble - lastTimeDouble) < minTime)
+		{
+		    minTime = timeDouble - lastTimeDouble;
+		}
+		lastTimeDouble = timeDouble;
+		i++;
             }
+            if(i == 100)
+	    {
+		int sum(0);
+		for (int j = 0 ; j < charVect.size() ; j++)
+		{
+		    printf("[%u] = %u\n", j, charVect[j]);
+		    sum = sum+charVect[j];
+		}
+		printf("\n");
+		printf("t_min = %fs\n",minTime);
+		double average = sum/difftime(time(NULL), originTime);
+		printf("average = %f key/s\n======\n", average);
+		i = 0;
+	    }
         }
         dev2.close();
     }
